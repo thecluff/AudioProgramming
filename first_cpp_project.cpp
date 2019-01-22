545330635933aa5ee0b2c9c71ca2be6e59c66d7f
Charlie Cluff <charlie_cluff@yahoo.com>
2019-01-22 12:00:19 -0800

    Age input

--- first_cpp_project.cpp
+++ first_cpp_project.cpp
@@ -4,6 +4,15 @@ using namespace std;
 // "main" is the point of entry for the program
 
 int main()
-{ cout << "Hello World" << endl;
+{ 
+
+// This is where the user's age will go
+int age; // This declares an integer variable with the name "age"    
+
+cout << "Hello World" << endl;
+cout << "Please enter your age." << endl;
+cin >> age;
+cout << "I see... your age is " << age << endl; 
+
 return 0;
 }
