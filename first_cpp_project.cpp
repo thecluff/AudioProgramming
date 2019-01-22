f6d5bce74f6443ea95463082b88d2eb1e12e7358
Charlie Cluff <charlie_cluff@yahoo.com>
2019-01-22 12:24:01 -0800

    local variable stuff

--- first_cpp_project.cpp
+++ first_cpp_project.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 // "main" is the point of entry for the program
+int my_func(int value) {
+    cout << value << endl;
+    // cout << age << endl;
+    return 0;
+}
 
 int main()
 { 
 // A function is a block of code that is configured with a name, a place to insert input values, and an output
-// These are local variables
+// These are local variables - meaning 
 // This is where the user's age will go
 int age; // This declares an integer variable with the name "age"    
 string name;
@@ -18,5 +23,6 @@ cout << "Please enter your name." << endl;
 cin >> name;
 cout << "I see, " << name << "... your age is " << age << endl; 
 
+my_func(100);
 return 0;
 }
