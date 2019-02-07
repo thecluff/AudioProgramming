620f60095c6faf5e542540f327cdf7bc35ba6843
Charlie Cluff <charlie_cluff@yahoo.com>
2019-02-07 12:02:08 -0800

    Powers of 2
    Powers of 2 and decibel equivalents

--- looping/tempCodeRunnerFile.cpp
+++ looping/tempCodeRunnerFile.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main (int argc, const char *argv[]) {
 
-int ndx = 0;
+int ndx = 100;
 // This is not an infinite loop
-while(ndx<=100) {
-    cout << ndx++ << endl;
+while(ndx>=0) {
+    cout << ndx-- << endl;
     // cout << (ndx = ndx + 1) << endl;
     // cout << (ndx += 1) << endl;
 
