db8db88e8d254a272f5269cfe7782bb03ce9c416
Charlie Cluff <charlie_cluff@yahoo.com>
2019-02-12 12:29:17 -0800

    Midi2Freq Lowest C

--- midi2freq/midifreq.cpp
+++ midi2freq/midifreq.cpp
@@ -20,10 +20,12 @@ int main(int argc, const char * argv[]) {
 
     const double magicInterval = pow(2,1.0/12);
     const double cAboveA440 = A440 * pow(magicInterval, 3);
+    const double lowestC = cAboveA440 * pow(2,-6);
 
     cout << setprecision(14) << "The twelfth root of 2 is " << magicInterval << endl;
     cout << setprecision(14) << "The frequency of Bb above A440 is " << A440 * magicInterval << endl;
     cout << setprecision(14) << "The frequency of C above A440 is " << cAboveA440 << endl;
-
+    cout << setprecision(14) << "The frequency of the lowest C is " << lowestC << endl;
+    
     return 0;
 }
