e37dc47b59a4ad4c6d5d5b9c894303194f7d6c98
Charlie Cluff <charlie_cluff@yahoo.com>
2019-03-26 12:27:47 -0700

    sndFile - add gain factor
    In terminal: ./testSndfile ../KungFu.wav KungFuCopy.wav 0.75

--- Sndfile1/testSndfile/testSndfile.cpp
+++ Sndfile1/testSndfile/testSndfile.cpp
@@ -1,6 +1,7 @@
 // testSndfile.cpp: copy the contents of one audio file to another
 
 #include <iostream>
+#include <cstdlib>
 #include <sndfile.h>
 
 // SNDFILE is a custom data type that belongs to the library libsndfile
@@ -28,15 +29,17 @@ using namespace std;
 
 int main(int argc, const char *argv[]) {
 
+    double gainFac = 1.0;
+
     cout << "If you are reading this, then the compiler must have found libsndfile." << endl;
 
-    if(argc<3) {
+    if(argc<4) {
         cout << "Please supply an input file and an output file" << endl;
         return 0;
     }
 
-    // Examine the two input arguments
-    cout << argv[1] << "\t\t" << argv[2] << endl;
+    // Examine the three input arguments
+    cout << argv[1] << "\t\t" << argv[2] << argv[3] << endl;
 
     // Open the input file. Opening files is "risky."
     // In other words, it might fail. For example: if the file cannot be found.
@@ -82,8 +85,10 @@ int main(int argc, const char *argv[]) {
     cout << "The number of samples read is " << "\t\t" << readCount << endl;
 
     // Copy audio from the input buffer to the output buffer
+    // Add a gain factor
+    gainFac = atof (argv[3]);
     for(int ndx=0;ndx<sfInInfo.frames*sfInInfo.channels;ndx++){
-        outp[ndx] = inp[ndx];
+        outp[ndx] = inp[ndx] * gainFac;
     }
 
     writeCount = sf_write_double(outfile, outp, sfInInfo.frames*sfInInfo.channels);
@@ -92,6 +97,7 @@ int main(int argc, const char *argv[]) {
 
     cout << "The number of input samples is " << "\t" << readCount << endl;
     cout << "The number of output samples is " << "\t" << writeCount << endl;
+
     // Clean up
     sf_close(infile);
     sf_close(outfile);
